e9b278fa7e0da4aa9d61b563535e357cbc6d5e49
Dāvis Namsons <davis.namsons@makit.lv>
2022-01-12 14:55:05 +0200

    Add a test to verify destination email extraction logic

--- spec/requests/rspec_spec.rb
+++ spec/requests/rspec_spec.rb
@@ -46,5 +46,18 @@ RSpec.describe 'rspec' do
         expect(response).to have_http_status :unauthorized
       end
     end
+
+    context 'with destination parameter set' do
+      let(:topic) { 'topic:arn:default' }
+
+      it 'extracts recipient email from SNS notification content' do
+        amazon_ingress_deliver_email(
+          mail: Mail.new,
+          message_params: { 'mail' => { 'destination' => 'user@example.com' } }
+        )
+
+        expect(ActionMailbox::InboundEmail.last.mail.recipients).to eql ['user@example.com']
+      end
+    end
   end
 end
